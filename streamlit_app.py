ac18aad050ef79d30aa576fc4806785f2c49ed7a
cd <cdagnino@gmail.com>
2020-12-12 12:26:14 -0300

    hello world de streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import numpy as np
+
+# Streamlit encourages well-structured code, like starting execution in a main() function.
+
+
+def main():
+    st.title("Holi")
+    st.sidebar.title("What to do")
+
+
+if __name__ == "__main__":
+    main()
