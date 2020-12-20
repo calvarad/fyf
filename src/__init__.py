4756197b7b454fe8b288e459e8004f47b6af4fe2
Carlos Alvarado <calvarado@uchicago.edu>
2020-12-19 23:37:29 -0300

    cambios bases

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,5 @@ from .utils import (generate_df_valores_cuota,
  cuadro_rentabilidades, get_annual_interest_rate)
 from .plots import (compare_distributions,
                     get_results_for_different_starting_days)
+from .crea_estrategia_optima import estrategia_optima
 
